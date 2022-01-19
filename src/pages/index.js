773ca3e6ea465d2fb4772884985b80b3b776e393
Firas <firas.jerbi.engineer@gmail.com>
2022-01-19 11:05:44 +0100

    Getting rid of the Loader

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 
 import Footer from "../components/Footer";
-
-import Loading from "../components/Loading";
 import Nav from "../components/Nav";
 
 import favicon from "../images/logojohndoe.png";
@@ -14,35 +12,24 @@ import Section4 from "../components/Section4";
 import Section5 from "../components/Section5";
 
 export default function Home() {
-  const [spinner, setSpinner] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => setSpinner(false), 3000);
-  }, []);
-
   return (
     <div className="flex flex-col dark:bg-gray-900">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
 
-      
         <link rel="icon" href={favicon} />
       </Head>
 
-      {spinner ? (
-        <Loading />
-      ) : (
-        <div className="flex flex-col dark:bg-gray-900">
-          <Nav />
-          <Section1 />
-          <Section2 />
-          <Section3 />
-          <Section4 />
-          <Section5 />
-          <Footer />
-        </div>
-      )}
+      <div className="flex flex-col dark:bg-gray-900">
+        <Nav />
+        <Section1 />
+        <Section2 />
+        <Section3 />
+        <Section4 />
+        <Section5 />
+        <Footer />
+      </div>
     </div>
   );
 }
