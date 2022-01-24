9cb443d98cc9c2a6e3d16f2a184b625102eb5d76
Firas <firas.jerbi.engineer@gmail.com>
2022-01-24 11:43:32 +0100

    fixing Navigation bug

--- src/components/Section3.js
+++ src/components/Section3.js
@@ -3,7 +3,7 @@ import data from "../utils/data.json";
 function Section3() {
   return (
     <>
-      <div className="dark:bg-gray-900 bg-[#F6F8FB] w-full" id="Section2">
+      <div className="dark:bg-gray-900 bg-[#F6F8FB] w-full" id="Section3">
         <div className="flex flex-col-reverse items-center justify-between px-8 pt-8 pb-16 md:flex-row sm:px-16">
           <div className="flex flex-col space-y-3 sm:space-y-6 items-start text-[#5C637C] xl:border-l-8 xl:pl-8 xl:border-gray-200 py-8">
             <h4 className=" mb-8 text-3xl font-extrabold md:flex sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">
